d207f1ead0626cbfa5a1d34f1fbae6a3a84d0c06
L Daniel De San Pedro V <ldanieldesanpedro@gmail.com>
2019-03-22 09:08:48 -0600

    Initial Commit

---
+++ Purga Time/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Purga Time
+//
+//  Created by L Daniel De San Pedro on 3/22/19.
+//  Copyright © 2019 L Daniel De San Pedro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Purga TimeTests/Purga_TimeTests.swift
@@ -0,0 +1,34 @@
+//
+//  Purga_TimeTests.swift
+//  Purga TimeTests
+//
+//  Created by L Daniel De San Pedro on 3/22/19.
+//  Copyright © 2019 L Daniel De San Pedro. All rights reserved.
+//
+
+import XCTest
+@testable import Purga_Time
+
+class Purga_TimeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
